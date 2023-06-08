e4676d299a52b4e87edd24e2c206b06f163a800a
Danny Burrow <danny@pdxcodeguild.com>
2023-06-08 15:10:56 -0700

    proof of concept for github pages support

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['@/assets/css/styles.css']
+    css: ['@/assets/css/styles.css'],
+    baseURL: '/'
 })
---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+    <h4>This page has authors, who we will tell you about someday</h4>
+</template>
