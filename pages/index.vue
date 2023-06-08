e1d9607a665aee7fe84826ae5f64bb7ffbb2f457
Danny Burrow <danny@pdxcodeguild.com>
2023-06-08 14:57:11 -0700

    proof of concept for nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>
+            This user's token is: {{ authtoken.token }}
+        </h1>
+    </div>
+</template>
+
+<script setup>
+
+const userData = { username: 'dan', password: '1' }
+const url = 'http://pyxishabits.pythonanywhere.com/accounts/auth/'
+
+const { data: authtoken } = useFetch(url, {
+        method: 'POST',
+        body: userData
+    });
+
+</script>
